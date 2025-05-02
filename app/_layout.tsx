2e74fd609d8f3ac5307cc310bca24bc6f045b4da
tianruipom <tianrui.lau@pom.com.sg>
2025-05-02 19:11:46 +0800

    init

---
+++ app/_layout.tsx
@@ -0,0 +1,38 @@
+import { LogtoConfig, LogtoProvider } from '@logto/rn';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  const config: LogtoConfig = {
+    endpoint: 'https://sso.pomstage.app/',
+    appId: 'hn2j3zpvn3rreoje4yx90',
+  };
+  
+
+  return (
+    <LogtoProvider config={config}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </LogtoProvider>
+  );
+}
